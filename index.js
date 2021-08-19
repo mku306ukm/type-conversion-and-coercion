242a2f8977bafe5d187f24b6458bfbb620e90824
umesh_kumar_mehta <kumarumesh15294@gmail.com>
2021-08-19 02:02:41 -0700

    type-conversion-and-coercion

---
+++ index.js
@@ -0,0 +1,24 @@
+console.log('hi')
+
+
+//type conversion
+const inputYear = '1994';
+console.log(Number(inputYear),inputYear);
+console.log(Number(inputYear) +27);
+
+console.log(Number('Umesh'));
+console.log(typeof NaN);
+
+console.log(String(23),23);
+
+
+//type coercion 
+console.log('i am ' + 27 + ' year old');
+console.log('23'- '10'-3);
+console.log('23'/'2');
+console.log('23'> '18');
+
+let n = '1' + 1; //11
+n = n - 1;
+console.log(n);
+
